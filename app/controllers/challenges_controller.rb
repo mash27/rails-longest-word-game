a0a930dccfbe3b305c6aa26a46db15f92357f094
Ash Madi <a.madi27@gmail.com>
2017-07-31 16:47:52 +0200

    done

---
+++ app/controllers/challenges_controller.rb
@@ -0,0 +1,56 @@
+class ChallengesController < ApplicationController
+  def game
+    @grid = generate_grid(9)
+    $start_time = Time.now
+  end
+
+  def generate_grid(grid_size)
+    Array.new(grid_size) { ('A'..'Z').to_a.sample }
+  end
+
+  def included?(attempt, grid)
+    attempt.chars.all? { |letter| attempt.count(letter) <= grid.count(letter) }
+  end
+
+  def run_game(attempt, grid, start_time, end_time)
+    result = { time: end_time - start_time }
+
+    score_and_message = score_and_message(attempt, grid, result[:time])
+    result[:score] = score_and_message.first
+    result[:message] = score_and_message.last
+
+    result
+  end
+
+  def score_and_message(attempt, grid, time)
+    if included?(attempt.upcase, grid)
+      if english_word?(attempt)
+        score = compute_score(attempt, time)
+        [score, "well done"]
+      else
+        [0, "not an english word"]
+      end
+    else
+      [0, "not in the grid"]
+    end
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
+
+
+    def result
+    $time_taken = (Time.now - $start_time).to_s
+    @attempt = params.fetch(:attempt)
+    grid = params.fetch(:grid)
+    $end_time =Time.now
+    @results = run_game(@attempt, grid, $start_time, $end_time)
+  end
+
+    def compute_score(attempt, time_taken)
+    time_taken > 60.0 ? 0 : attempt.size * (1.0 - time_taken / 60.0)
+  end
+end
